95f7152a86c976be1271d4fddeb9a7e4b5c9b025
fracab <f.caberlin@gmail.com>
2018-05-22 12:17:39 +0200

    new code and plots

--- HQAICleancode.R
+++ HQAICleancode.R
@@ -35,6 +35,7 @@ boxHQAI2<-ggplot(df, aes(Organization, Score))+
 p2<-ggplotly(boxHQAI1)
 p3<-ggplotly(boxHQAI2) 
 
+
 #Radar plot
 
 dfAV<-df[df$Organization=='Average',]
@@ -45,9 +46,8 @@ radar<-ggplot(dfNOAV, aes(x=Commitment, y=Score, group=Organization, color=Organ
   facet_wrap(~ Organization) + coord_polar() + geom_polygon(aes(y = dfAV$Score, x=Commitment), color="black", show.legend=FALSE, fill=NA)+
 theme(text = element_text(size=20))
 
-#Radar plot Coast Trust
-dfCT<-df[df$Organization=='Coast Trust',]
 
+# short dataframe with averages for comparison with single organizations
 '%!in%' <- function(x,y)!('%in%'(x,y))
   dfAVold<-df[df$Organization %!in% c('Women Support Association', 'Plan Int', 'Caritas DK', 'Coast Trust','Average'),]
 dfAVold<-aggregate(dfAVold[, 3], list(dfAVold$Commitment), mean)
@@ -55,12 +55,46 @@ dfAVold$Organization<-"OldAverage"
 colnames(dfAVold)<-c("Commitment","Score","Organization")
 dfAVshort<-dfAV[1:9,]
 
-radarCoast<-ggplot(dfCT, aes(x=Commitment, y=Score, group=Organization, color=Organization)) +
-  geom_polygon (aes(fill=Organization),show.legend = FALSE, alpha=0.5) + ylim (0,5) + theme_bw() +
-  coord_polar() + geom_polygon(aes(y = dfAVshort$Score, x=Commitment), color="black", show.legend=FALSE, fill=NA)+
-  theme(text = element_text(size=20))+ geom_polygon(aes(y = dfAVold$Score, x=Commitment), color="red", show.legend=FALSE, fill=NA)+
-  theme(text = element_text(size=20))
+
+#Radar plot Coast Trust
+dfCT<-df[df$Organization=='Coast Trust',]
+dfCT <-rbind(dfCT, dfAVshort, dfAVold)
+
+radarCT<-ggplot(dfCT, aes(x=Commitment, y=Score, group=Organization, color=Organization, fill=Organization, alpha=Organization)) +
+  geom_polygon (aes(),show.legend = TRUE) + ylim (0,5) + theme_bw() +
+  coord_polar() + ggtitle ("Coast Trust") + 
+  scale_color_manual(values = c("red","blue","black")) +scale_fill_manual(values = c("red","red","black"))+ scale_alpha_manual(values = c(0,0.5,0))
 
 #Radar plot Women Support Association
-dfWSA<-df[df$Organization=='Women Support Association']
+dfWSA<-df[df$Organization=='Women Support Association',]
+dfWSA <-rbind(dfWSA, dfAVshort, dfAVold)
 
+radarWSA<-ggplot(dfWSA, aes(x=Commitment, y=Score, group=Organization, color=Organization, fill=Organization, alpha=Organization)) +
+  geom_polygon (aes(),show.legend = TRUE) + ylim (0,5) + theme_bw() +
+  coord_polar() + ggtitle ("Women Support Association") + 
+  scale_color_manual(values = c("red","green","black")) +scale_fill_manual(values = c("red","yellow","black"))+ scale_alpha_manual(values = c(0,0.5,0))
+
+#Radar plot Plan International
+dfPlan<-df[df$Organization=='Plan Int',]
+dfPlan <-rbind(dfPlan, dfAVshort, dfAVold)
+
+radarPlan<-ggplot(dfPlan, aes(x=Commitment, y=Score, group=Organization, color=Organization, fill=Organization, alpha=Organization)) +
+  geom_polygon (aes(),show.legend = TRUE) + ylim (0,5) + theme_bw() +
+  coord_polar() + ggtitle ("Plan International") + 
+  scale_color_manual(values = c("red","blue","black")) +scale_fill_manual(values = c("red","blue","black"))+ scale_alpha_manual(values = c(0,0.3,0))
+
+#plan and plan de
+dfPlanPlan<-rbind(df[df$Organization=="Plan Int",],df[df$Organization=="Plan Int DE",])
+  
+radarPlanPlan<-ggplot(dfPlanPlan, aes(x=Commitment, y=Score, group=Organization, color=Organization)) +
+  geom_polygon (aes(fill=Organization), alpha=0.3) +  ylim (0,5) + theme_bw() +
+  coord_polar() + ggtitle ("Plan International and Plan International DE")
+
+#Radar plot Caritas DK
+dfCarDK<-df[df$Organization=='Caritas DK',]
+dfCarDK <- rbind(dfCarDK, dfAVshort, dfAVold) 
+
+radarCaritasDK<-ggplot(dfCarDK, aes(x=Commitment, y=Score, group=Organization, color=Organization, fill=Organization, alpha=Organization)) +
+  geom_polygon (aes(),show.legend = TRUE) + ylim (0,5) + theme_bw() +
+  coord_polar() + ggtitle ("Caritas DK") + 
+  scale_color_manual(values = c("red","pink","black")) +scale_fill_manual(values = c("red","red","black"))+ scale_alpha_manual(values = c(0,0.5,0))
